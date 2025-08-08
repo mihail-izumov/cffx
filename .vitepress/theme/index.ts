125eaa93305813b6eea8d212a21888562ac106bc
mihail-izumov <106297169+mihail-izumov@users.noreply.github.com>
2025-08-08 09:04:42 +0400

    Update index.ts

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,10 +1,17 @@
 import { h } from 'vue'
+import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+import SimulatorCards from '../components/SimulatorCards.vue' // 1. ПРИКАЗ: НАЙТИ ОРУЖЕЙНИКА
 
 export default {
   extends: DefaultTheme,
   Layout() {
     return h(DefaultTheme.Layout, null, {
+      // Здесь могут быть слоты для кастомизации макета, если понадобятся
     })
+  },
+  enhanceApp({ app }) {
+    // 2. ПРИКАЗ: ДАТЬ ДОСТУП В ЛЮБОЙ ЗАЛ
+    app.component('SimulatorCards', SimulatorCards)
   }
-}
+} satisfies Theme
