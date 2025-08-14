9063afb0cd7a881786edb96f90f11d7fa3cabb91
mihail-izumov <106297169+mihail-izumov@users.noreply.github.com>
2025-08-14 15:08:50 +0400

    Update index.ts

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,19 +1,20 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
+
 import SimulatorCards from '../components/SimulatorCards.vue'
 import BrandCards from '../components/BrandCards.vue'
+import FeaturesGrid from '../components/FeaturesGrid.vue'
 
 export default {
   extends: DefaultTheme,
   Layout() {
     return h(DefaultTheme.Layout, null, {
-      // Здесь могут быть слоты для кастомизации макета, если понадобятся
     })
   },
   enhanceApp({ app }) {
-    // Регистрация компонентов, чтобы они были доступны глобально
     app.component('SimulatorCards', SimulatorCards)
     app.component('BrandCards', BrandCards)
+    app.component('FeaturesGrid', FeaturesGrid)
   }
 } satisfies Theme
