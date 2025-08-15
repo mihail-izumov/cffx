2ee20266ea8f63b61b2797f65e028035ccef96c0
mihail-izumov <106297169+mihail-izumov@users.noreply.github.com>
2025-08-15 14:30:05 +0400

    Update index.ts

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,7 +1,7 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-// Импортируем каждый компонент с уни-кальным именем
+// Импортируем каждый компонент с уникальным именем
 import SimulatorCards from '../components/SimulatorCards.vue'
 import BrandCards from '../components/BrandCards.vue'
 import FeaturesGrid from '../components/FeaturesGrid.vue'
@@ -10,6 +10,7 @@ import InfoCardsPro from '../components/InfoCardsPro.vue'
 import ValueCards from '../components/ValueCards.vue'
 import PricingTable from '../components/PricingTable.vue'
 import MembershipCards from '../components/MembershipCards.vue'
+import TariffFeatures from '../components/TariffFeatures.vue'
 
 export default {
   extends: DefaultTheme,
@@ -27,5 +28,6 @@ export default {
     app.component('ValueCards', ValueCards)
     app.component('PricingTable', PricingTable)
     app.component('MembershipCards', MembershipCards)
+    app.component('TariffFeatures', TariffFeatures)
   }
 } satisfies Theme
