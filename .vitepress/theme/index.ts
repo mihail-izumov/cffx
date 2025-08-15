42e35cdc7cbf1272e72139d59bc0cf037de93fb8
mihail-izumov <106297169+mihail-izumov@users.noreply.github.com>
2025-08-15 11:39:33 +0400

    Update index.ts

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -8,7 +8,8 @@ import FeaturesGrid from '../components/FeaturesGrid.vue'
 import InstrumentsGrid from '../components/InstrumentsGrid.vue'
 import InfoCardsPro from '../components/InfoCardsPro.vue'
 import ValueCards from '../components/ValueCards.vue'
-import PricingTable from '../components/PricingTable.vue' // <-- ДОБАВЛЕНО
+import PricingTable from '../components/PricingTable.vue'
+import MembershipCards from '../components/MembershipCards.vue'
 
 export default {
   extends: DefaultTheme,
@@ -24,6 +25,7 @@ export default {
     app.component('InstrumentsGrid', InstrumentsGrid)
     app.component('InfoCardsPro', InfoCardsPro)
     app.component('ValueCards', ValueCards)
-    app.component('PricingTable', PricingTable) // <-- ДОБАВЛЕНО
+    app.component('PricingTable', PricingTable)
+    app.component('MembershipCards', MembershipCards)
   }
 } satisfies Theme
