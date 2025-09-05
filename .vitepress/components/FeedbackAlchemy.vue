45708c726702bdf415395aa46351a2351a7b84f2
mihail-izumov <106297169+mihail-izumov@users.noreply.github.com>
2025-09-05 14:19:16 +0400

    Update FeedbackAlchemy.vue

--- .vitepress/components/FeedbackAlchemy.vue
+++ .vitepress/components/FeedbackAlchemy.vue
@@ -94,7 +94,7 @@
       <div class="form-footer">
         <div class="checkbox-group">
           <input type="checkbox" id="consent" v-model="form.consent" required>
-          <label for="consent">Согласен/на с <a href="/terms" target="_blank" class="policy-link">Условиями использования</a>.</label>
+          <label for="consent">С<a href="/terms" target="_blank" class="policy-link">Условиями использования</a> согласен/на</label>
         </div>
         <button type="submit" class="submit-btn" :disabled="!isFormValid || isSubmitting">
           {{ isSubmitting ? 'Отправка...' : 'Отправить Сигнал' }}
