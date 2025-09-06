94ab78e301e61a2ea9bdf05d4b52550a128056bb
mihail-izumov <106297169+mihail-izumov@users.noreply.github.com>
2025-09-06 18:11:41 +0400

    Update PulseSubscribeBanner.vue

--- .vitepress/components/PulseSubscribeBanner.vue
+++ .vitepress/components/PulseSubscribeBanner.vue
@@ -28,7 +28,7 @@ import { defineProps, defineEmits } from 'vue'
 const props = defineProps({
   imageSrc: {
     type: String,
-    default: '/brew_intro_ban.jpg'
+    default: '/brew_outro_ban.jpg'
   },
   imageAlt: {
     type: String,
