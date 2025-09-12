f9ee8888f2dfdde332b37c275a2f222c812b591a
mihail-izumov <106297169+mihail-izumov@users.noreply.github.com>
2025-09-12 21:10:28 +0400

    Update CustomLayout.vue

--- .vitepress/theme/CustomLayout.vue
+++ .vitepress/theme/CustomLayout.vue
@@ -1,38 +1,17 @@
 <script setup>
-import { computed, watch } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import { useData } from 'vitepress'
 import NotificationSlider from './NotificationSlider.vue'
 import GeneralNotification from './GeneralNotification.vue'
 
 const { Layout } = DefaultTheme
-const { frontmatter, route } = useData()
-
-// Вычисляемое свойство, чтобы определить, нужно ли показывать баннер
-const shouldShowBanner = computed(() => {
-  return frontmatter.value.notification === 'brew' || frontmatter.value.notification === 'general'
-})
-
-// Функция для обновления класса на <html>
-const updateHtmlClass = () => {
-  if (typeof document !== 'undefined') {
-    if (shouldShowBanner.value) {
-      document.documentElement.classList.add('has-notification-banner')
-    } else {
-      document.documentElement.classList.remove('has-notification-banner')
-    }
-  }
-}
-
-// Следим за изменением пути и немедленно вызываем функцию.
-// { immediate: true } гарантирует, что класс будет установлен/удален при первой же загрузке компонента,
-// решая проблему "дырки" при навигации.
-watch(() => route.path, updateHtmlClass, { immediate: true })
+const { frontmatter } = useData()
 </script>
 
 <template>
   <Layout>
     <template #layout-top>
+      <!-- Условный рендеринг в самом верхнем слоте -->
       <NotificationSlider v-if="frontmatter.notification === 'brew'" />
       <GeneralNotification v-else-if="frontmatter.notification === 'general'" />
     </template>
