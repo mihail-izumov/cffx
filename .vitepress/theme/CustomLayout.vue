b66ae0d7fd48083b3220264096df2d9b1e03f2dc
mihail-izumov <106297169+mihail-izumov@users.noreply.github.com>
2025-09-12 19:15:58 +0400

    Update CustomLayout.vue

--- .vitepress/theme/CustomLayout.vue
+++ .vitepress/theme/CustomLayout.vue
@@ -1,21 +1,39 @@
-<!-- .vitepress/theme/CustomLayout.vue -->
 <script setup>
+import { ref, onMounted, watch } from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import { useData } from 'vitepress'
 import NotificationSlider from './NotificationSlider.vue'
 import GeneralNotification from './GeneralNotification.vue'
 
 const { Layout } = DefaultTheme
-// Теперь используем frontmatter вместо route
-const { frontmatter } = useData()
+const { frontmatter, route } = useData()
+
+// Ref для элемента баннера, чтобы получить его высоту
+const notificationRef = ref(null)
+
+// Функция для обновления отступа
+const updatePadding = () => {
+  if (notificationRef.value) {
+    const height = notificationRef.value.offsetHeight
+    document.documentElement.style.setProperty('--vp-layout-top-height', `${height}px`)
+  } else {
+    document.documentElement.style.setProperty('--vp-layout-top-height', '0px')
+  }
+}
+
+// Вызываем функцию при монтировании и при смене страницы
+onMounted(updatePadding)
+watch(() => route.path, updatePadding)
 </script>
 
 <template>
   <Layout>
     <template #layout-top>
-      <!-- Проверяем флаг из frontmatter -->
-      <NotificationSlider v-if="frontmatter.notification === 'brew'" />
-      <GeneralNotification v-else-if="frontmatter.notification === 'general'" />
+      <div ref="notificationRef">
+        <!-- Оборачиваем баннеры в div с ref -->
+        <NotificationSlider v-if="frontmatter.notification === 'brew'" />
+        <GeneralNotification v-else-if="frontmatter.notification === 'general'" />
+      </div>
     </template>
   </Layout>
 </template>
