ad8df63835622e8b267f1e9c29c2d936b8ee3c3d
mihail-izumov <106297169+mihail-izumov@users.noreply.github.com>
2025-10-28 15:14:43 +0400

    Update SignalSuperSpecsSlider.vue

--- .vitepress/components/SignalSuperSpecsSlider.vue
+++ .vitepress/components/SignalSuperSpecsSlider.vue
@@ -13,7 +13,6 @@
         role="link"
       >
         <div class="sss-card-background-icon">
-          <!-- Вставляем SVG напрямую как template, выбирая по индексу -->
           <svg v-if="idx === 0" xmlns="http://www.w3.org/2000/svg" width="300" height="300" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 4.1 12 6"/><path d="m5.1 8-2.9-.8"/><path d="m6 12-1.9 2"/><path d="M7.2 2.2 8 5.1"/><path d="M9.037 9.69a.498.498 0 0 1 .653-.653l11 4.5a.5.5 0 0 1-.074.949l-4.349 1.041a1 1 0 0 0-.74.739l-1.04 4.35a.5.5 0 0 1-.95.074z"/></svg>
           <svg v-else-if="idx === 1" xmlns="http://www.w3.org/2000/svg" width="300" height="300" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect width="18" height="14" x="3" y="5" rx="2" ry="2"/><path d="M7 15h4"/><path d="M15 15h2"/><path d="M7 11h2"/><path d="M13 11h4"/></svg>
           <svg v-else-if="idx === 2" xmlns="http://www.w3.org/2000/svg" width="300" height="300" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M22 17a2 2 0 0 1-2 2H6.828a2 2 0 0 0-1.414.586l-2.202 2.202A.71.71 0 0 1 2 21.286V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2z"/><path d="M7 11h10"/><path d="M7 15h6"/><path d="M7 7h8"/></svg>
@@ -49,7 +48,6 @@
     </div>
   </div>
 </template>
-
 <script setup>
 import { ref } from 'vue'
 const activeSlide = ref(null)
