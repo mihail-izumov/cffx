f3156035a817ed1d59e4452746192ca23e2ff342
mihail-izumov <106297169+mihail-izumov@users.noreply.github.com>
2025-10-31 13:01:19 +0400

    Update DialogsWidgets.vue

--- .vitepress/components/DialogsWidgets.vue
+++ .vitepress/components/DialogsWidgets.vue
@@ -11,8 +11,8 @@
             </svg>
           </span>
         </a>
-        <a href="/pro/specs" class="btn-see-all">
-          Спецификации и цены
+        <a href="/pro/ltvcalc" class="btn-see-all">
+          LTV Калькулятор
           <span class="icon-circle">
             <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
               <path d="M5 12h14"/>
