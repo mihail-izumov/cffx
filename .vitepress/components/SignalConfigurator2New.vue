0d9cd9bbf663592a01e98ad777660110bb422c53
mihail-izumov <106297169+mihail-izumov@users.noreply.github.com>
2025-11-20 14:39:15 +0400

    Update SignalConfigurator2New.vue

--- .vitepress/components/SignalConfigurator2New.vue
+++ .vitepress/components/SignalConfigurator2New.vue
@@ -1254,7 +1254,7 @@ onUnmounted(() => {
   padding: 1.25rem;
   border: 1px solid #3a3a3e;
   border-left: 4px solid var(--accent-color, #444);
-  padding-bottom: 50px;
+  padding-bottom: 35px;
 }
 
 .signal-question-block.contact {
