85fcb6a0ef722db8bf12bf6f4e2fc207db8df3f8
mihail-izumov <106297169+mihail-izumov@users.noreply.github.com>
2025-11-28 19:38:54 +0400

    Update BadgeCounter.vue

--- .vitepress/components/BadgeCounter.vue
+++ .vitepress/components/BadgeCounter.vue
@@ -42,7 +42,7 @@ onUnmounted(() => {
       </svg>
       
       <span class="signal-text">
-        {{ count }} оценок и Сигналов отправлено сегодня
+        {{ count }} оценок и сигналов отправлено сегодня
       </span>
     </div>
   </div>
