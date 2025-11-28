cc372cb779a4a7395b1d98c4e123e5849902b46b
mihail-izumov <106297169+mihail-izumov@users.noreply.github.com>
2025-11-28 11:13:18 +0400

    Update MainSignalWidgetsCoffee.vue

--- .vitepress/components/MainSignalWidgetsCoffee.vue
+++ .vitepress/components/MainSignalWidgetsCoffee.vue
@@ -8,7 +8,7 @@ const cafeNames = ['Корж', 'MOSAIC', 'Surf', 'Skuratov', 'Белотурка
 const cafes = {
   'Корж': {
     name: 'Корж',
-    yandex2gis: Подключены,
+    yandex2gis: 67,
     yandex2gisPercent: 94,
     smartReviews: 0,
     smartReviewsPercent: 0,
