69c84195190192aeb84d0692c18bcc9a046a4fef
mihail-izumov <106297169+mihail-izumov@users.noreply.github.com>
2025-12-06 11:59:12 +0400

    Update InvestKorzhInfoBlock.vue

--- .vitepress/components/InvestKorzhInfoBlock.vue
+++ .vitepress/components/InvestKorzhInfoBlock.vue
@@ -213,7 +213,7 @@ const formattedViews = computed(() => formatNumber(stats.value.pageViewsKorzh))
           </div>
 
           <div class="share-btn-circle telegram" @click="shareTelegram">
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="22" y1="2" x2="11" y2="13"/><polygon points="22 2 15 22 11 13 2 9 22 2"/></svg>
+            <img src="/telegram-icon.svg" alt="Telegram" width="24" height="24" />
           </div>
         </div>
       </div>
