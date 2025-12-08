117556c6c88090483e3e126a58ede27e76243f2a
mihail-izumov <106297169+mihail-izumov@users.noreply.github.com>
2025-12-08 15:56:54 +0400

    Update SignalFormKorzh1.vue

--- .vitepress/components/SignalFormKorzh1.vue
+++ .vitepress/components/SignalFormKorzh1.vue
@@ -241,8 +241,8 @@
   :ticket="formattedTicketNumber"
   :date="currentDate.split(' ')[0]"
   :address="form.coffeeShopAddress"
-  :emotional-text="form.emotional"      <!-- поле эмоций -->
-  :problem-text="form.detailsCleaned"   <!-- собранный текст сути -->
+  :emotion-text="form.emotionalRelease"
+  :problem-text="form.factualAnalysis"
 />
 
   </div> <!-- Закрытие signal-form-wrapper -->
