1e5407f48bca3bd3f6242bea29c6505cd6aa0459
mihail-izumov <106297169+mihail-izumov@users.noreply.github.com>
2025-12-17 19:44:22 +0400

    Update SignalFormKorzhCard1.vue

--- .vitepress/components/SignalFormKorzhCard1.vue
+++ .vitepress/components/SignalFormKorzhCard1.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { reactive, ref, computed, onMounted, onUnmounted, watch } from 'vue'
-import KorzhBadgeCardGenerator from './KorzhBadgeCardGenerator'
+import KorzhBadgeCardGenerator from './KorzhBadgeCardGenerator.vue'
 
 // Данные формы
 const form = reactive({
