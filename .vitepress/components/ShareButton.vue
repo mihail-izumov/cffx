0aed9f7d5f0847622d28903dc919616f3c1dfd9c
mihail-izumov <106297169+mihail-izumov@users.noreply.github.com>
2025-12-18 19:20:53 +0400

    Update ShareButton.vue

--- .vitepress/components/ShareButton.vue
+++ .vitepress/components/ShareButton.vue
@@ -139,9 +139,16 @@ onUnmounted(() => {
 </template>
 
 <style scoped>
+
 .share-only {
   display: inline-block;
+  min-height: 100vh;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
+
 
 /* КНОПКА */
 .btn-share {
